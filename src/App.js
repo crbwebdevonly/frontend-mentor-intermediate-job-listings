2d545422845f31c0bed24a350795f8369f988fad
crbwebdevonly <crbwebdevonly@gmail.com>
2021-10-28 21:43:46 +0100

    deploy success1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>job listing-test1 deploy1</h1>
+     <h1>job listing-test1 deploy2</h1>
     </div>
   );
 }
 
 export default App;
+
+
+// git remote set-url origin https://github.com/crbwebdevonly/frontend-mentor-intermediate-job-listings
